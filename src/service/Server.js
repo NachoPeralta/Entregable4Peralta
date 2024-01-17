5a0c1fb622b007b77e6ffc1236017f69fed66467
wiperalta <wiperalta@gmail.com>
2024-01-17 09:49:18 -0300

    problema de conexion cliente servidor solucionado

--- src/service/Server.js
+++ src/service/Server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const productsRouter = require("../routes/products.router");
-const cartRouter     = require("../routes/cart.router.js");
+const cartRouter = require("../routes/cart.router.js");
 const exphbs = require("express-handlebars");
 const socket = require("socket.io");
 const viewsRouter = require("../routes/views.router.js");
+const path = require("path");
 
 class Server {
     // Se crea una instancia de express para crear el servidor.
@@ -13,36 +14,33 @@ class Server {
     }
 
     // Se crea un método para levantar el servidor al iniciar la aplicación.
-  async start() {
-        this.app.use(express.static("./public"));
+    async start() {
+        this.app.use(express.static(path.join(__dirname, "../public")));
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
 
-        this.app.use("/api/products", productsRouter); 
-        this.app.use("/api/carts", cartRouter);
-        this.app.use("/", viewsRouter);
-
-        // Configuracion de motor de plantilla
+        // Configuracion de motor de plantilla y handlebars
         this.app.engine("handlebars", exphbs.engine());
         this.app.set("view engine", "handlebars");
         this.app.set("views", "./src/views");
 
-        const httpServer = this.app.listen(this.port, () => {
-            // Se corroborará que el servidor esté corriendo en el puerto 8080.
-            console.log(`Servidor escuchando en puerto: ${this.port}`);
-        });
-        
-        const io = socket(httpServer);
-        
+        // Routing
+        this.app.use("/api/products", productsRouter);
+        this.app.use("/api/carts", cartRouter);
+        this.app.use("/", viewsRouter);
+
+        const httpServer = this.app.listen(this.port);
+
+        const io = new socket.Server(httpServer);
+
         io.on("connection", (socket) => {
             console.log("Nuevo cliente conectado");
-            
+
             socket.on("message", data => {
                 console.log(data);
                 io.emit("respuesta", "mensaje recibido");
             })
         });
-    
     }
 }
 module.exports = Server;
